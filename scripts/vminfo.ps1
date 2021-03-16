753baa0712a784f6beecf4bc63574118973a35a7
Dh3va <alessandro.pranzo@gmail.com>
2021-03-16 20:18:23 +0100

    First commit

--- scripts/vminfo.ps1
+++ scripts/vminfo.ps1
@@ -3,3 +3,5 @@
 Connect-VIServer $server -User $username -Password $password
 
 Get-Cluster $cluster | Get-VM | Select Name,PowerState,Version,Guest,NumCpu,MemoryMB,MemoryGB | Sort Name | Export-CSV $user_path".csv"
+
+Write-Host "test"
