80d4abb67f8b5d3aca826075aa8d58f3e16b5b0a
Dh3va <alessandro.pranzo@gmail.com>
2021-03-16 20:28:37 +0100

    vminfo

--- scripts/vminfo.ps1
+++ scripts/vminfo.ps1
@@ -3,5 +3,3 @@
 Connect-VIServer $server -User $username -Password $password
 
 Get-Cluster $cluster | Get-VM | Select Name,PowerState,Version,Guest,NumCpu,MemoryMB,MemoryGB | Sort Name | Export-CSV $user_path".csv"
-
-Write-Host "test"
